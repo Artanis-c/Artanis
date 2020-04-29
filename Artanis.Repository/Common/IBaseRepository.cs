47bf88dd5e1adcba1147df0fd14d917e1219eefb
cui <291527070@qq.com>
2020-04-29 09:41:50 +0800

    修复文件路径

--- Artanis.Repository/Common/IBaseRepository.cs
+++ Artanis.Repository/Common/IBaseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Artanis.Model.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace Artanis.Repository.Common
 {
     public interface IBaseRepository<T> 
     {
-
+        ResponseModel<int> Insert(T entityu);
     }
 }
